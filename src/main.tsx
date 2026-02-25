483758b0d398ceab9feae7430555f9b2f38aafb3
Josué Lustosa <josuelustosa.job@gmail.com>
2026-02-25 15:19:09 -0400

    refactor(routes): Centralize your routing system with createBrowserRouter.

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,12 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { RouterProvider } from "react-router-dom"
 
-import "./index.css"
-import { App } from "./App.tsx"
-
-// pages
-import { Dashboard } from "./pages/Dashboard.tsx"
-import { Register } from "./pages/Register.tsx"
-import { History } from "./pages/History.tsx"
-import { About } from "./pages/About.tsx"
+// routing system
+import { router } from "@/router"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route path="*" element={<h1>Not Found</h1>} />
-          <Route index element={<Dashboard />} />
-          <Route path="/registrar-acerto" element={<Register />} />
-          <Route path="/historico-de-acertos" element={<History />} />
-          <Route path="/sobre" element={<About />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>
 )
