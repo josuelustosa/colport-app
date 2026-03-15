ee0369269913fe454340a9ef7e4edbf15d41ad25
Josué Lustosa <josuelustosa.job@gmail.com>
2026-03-15 14:18:33 -0400

    feat(design-system): typography and spacing

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,10 @@ export function App() {
 
       <main className="max-w-5xl mx-auto px-6 py-8">
         {/* centralized routing system */}
-        <Outlet /> 
+        <Outlet />
+        <h2>Sou um Título - H2</h2>
+        <h3>Sou um Título - H3</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum explicabo ipsa dolorem soluta aut laboriosam recusandae sequi o officia aliquid ipsum? Sequi autem laborum mollitia corporis similique excepturi sunt. Quasi, dolor.</p>
       </main>
     </PageContainer>
   )
